dd90947c07bb17d9cf3b39915b1eb1beb5dba4ea
sriram <sriramalavalapatiit01@gmail.com>
2022-09-13 22:59:07 +0530

    program

---
+++ problem1.js
@@ -0,0 +1,13 @@
+// Check whether a number is Prime or not
+
+let num=2
+for(let i=1;i<=num;i++)
+{
+if(num%2!==0)
+{
+console.log("num is a prime");
+}else
+{
+console.log("number is not a prime");
+}
+}
---
+++ problem2.js
@@ -0,0 +1,16 @@
+// Check whether a string is palindrome or not.
+let str="aabbcc";
+let res="";
+for(let i=0;i<=str.length-1)
+{
+res+=str[i];
+}
+if(res==str)
+{
+console/log("str is a palindrom");
+}
+else
+{
+console.log("str is not a palindrome");
+}
+}
