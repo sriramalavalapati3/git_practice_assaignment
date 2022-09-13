26b3ac6114c9cd599c1792f56b73eb208ae58ad4
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2022-09-14 00:00:51 +0530

    Right update

--- problem1.js
+++ problem1.js
@@ -2,14 +2,13 @@
 
 let num=17, count=0;
 
-for(let i=2;i<num;i++)
-{
-if(num%i==0)
-{
-count++;
-}
+for(let i=2;i<num;i++){
+    if(num%i==0){
+        count++;
+    }
 
 }
+
 if (count>0){
     console.log("Not a Prime Number");
 }
